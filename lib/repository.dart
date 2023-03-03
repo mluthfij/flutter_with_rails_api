a9b604814b3ff148d7c09a8d2afe468c32963246
mluthfij <luthfujuliansyah48@gmail.com>
2023-03-03 10:09:36 +0700

    post problem

--- lib/repository.dart
+++ lib/repository.dart
@@ -3,9 +3,8 @@ import 'package:article_app_flutter/model.dart';
 import 'package:http/http.dart' as http;
 
 class Repository {
+  // final _baseUrl = 'https://63fd81456a135172e071c8c6.mockapi.io/api/v1';
   final _baseUrl = 'http://10.0.2.2:3000/api/v1';
-  // final _baseUrl =
-  //     'https://63fd81456a135172e071c8c6.mockapi.io/api/v1/articles';
 
   Future getData() async {
     try {
